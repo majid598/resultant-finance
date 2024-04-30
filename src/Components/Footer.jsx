3df3900ed847807eed87e312cba40f023b0e3865
majid598 <143610202+majid598@users.noreply.github.com>
2024-04-30 10:58:47 +0630

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,15 +4,15 @@ import { FaLinkedin } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-[#1F1F1F] text-[#E1E1E1] relative z-[9] lg:px-52 px-10 lg:py-0 md:py-0 py-4">
+    <div className="w-full bg-[#1F1F1F] text-[#E1E1E1] relative z-[9] lg:px-52 md:px-20 sm:px-10 px-5 lg:py-0 md:py-0 py-4">
       <div className="w-full flex justify-between items-center lg:text-xl">
         <div className="flex items-center">
-          <div className="lg:h-40 md:h-40 sm:h-32 h-24">
+          <div className="lg:h-40 md:h-40 sm:h-32 h-20">
             <img src="/assets/1.png" className="h-full" alt="" />
           </div>
           <h1 className="lg:text-2xl md:text-2xl sm:text-lg font-semibold">&copy; Rsultant .</h1>
         </div>
-        <div className="flex gap-4 text-2xl">
+        <div className="flex gap-4 lg:text-2xl md:text-2xl sm:text-xl text-lg">
           <FaTwitter />
           <FaFacebookSquare />
           <FaLinkedin />
